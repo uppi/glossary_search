2079b1cc296413192417dd316603c61e622de704
uppi <helendile@ispras.ru>
2014-04-02 18:01:58 +0400

    Added exception handling

--- src/search.py
+++ src/search.py
@@ -14,34 +14,34 @@ from glossary import Glossary
 
 def main():
     import sys
-
-    glossary = Glossary()
- 
     app = QApplication(sys.argv)
+    parent = QDialog()
  
-    form = Form(glossary)
-    form.show()
+    try:
+        glossary = Glossary()
+        form = Form(glossary)
+        form.show()
+        parent = form
 
-    form.inputTextEdit.setDisabled(True);
-    form.inputTextEdit.setPlainText("Parsing... ");
-    fileName = './glossary_xls.xls'
-    try:
-        with open(fileName, "r") as f:
-            pass
-    except:
-        fileName = QFileDialog.getOpenFileName(form, "Open glossary", ".", "glossary (*.xls)");
-    try:
+        form.inputTextEdit.setDisabled(True);
+        form.inputTextEdit.setPlainText("Parsing... ");
+        fileName = './glossary_xls.xls'
         try:
-            glossary.parse_xls(fileName)
+            with open(fileName, "r") as f:
+                pass
         except:
-            glossary.parse_xls(fileName[0])
-        glossary.make_index()
-        form.inputTextEdit.setPlainText("");
-        form.inputTextEdit.setDisabled(False);
+            fileName = QFileDialog.getOpenFileName(form, "Open glossary", ".", "glossary (*.xls *.xlsx)");
+            try:
+                glossary.parse_xls(fileName)
+            except:
+                glossary.parse_xls(fileName[0])
+            glossary.make_index()
+            form.inputTextEdit.setPlainText("");
+            form.inputTextEdit.setDisabled(False);
+        sys.exit(app.exec_())
     except Exception as e:
-        form.inputTextEdit.setPlainText("Error: " + str(e));
-        raise
-    sys.exit(app.exec_())
+        QMessageBox.critical(parent, "Error", "Exception thrown: " + str(e))
+        sys.exit(app.quit())
 
 if __name__ == '__main__':
     main()
